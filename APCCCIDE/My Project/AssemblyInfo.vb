e35d22ebbc503d242cd95c1a2c501945a329cd55
ibuki2003 <ibuki426.sp@gmail.com>
2018-04-06 16:33:43 +0900

    Release 1.0.0

--- APCCCIDE/My Project/AssemblyInfo.vb
+++ APCCCIDE/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.3.3.0")>
-<Assembly: AssemblyFileVersion("0.3.3.0")>
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: NeutralResourcesLanguage("ja-JP")>
